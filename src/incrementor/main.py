238a9097e40b498a2dd336b3d3c9cbf5ac9bf0ce
Alexander Senetcky <Alexander.Senetcky@ct.gov>
2025-07-09 16:23:36 -0400

    feat: format and stack data and newdata

--- src/incrementor/main.py
+++ src/incrementor/main.py
@@ -14,7 +14,6 @@ from datetime import datetime as dt
 
 
 def snapshot_acs_source():
-
     raw = portal.fetch_data()
 
     orig_endpoints = (
@@ -28,7 +27,11 @@ def snapshot_acs_source():
 
     max_row_id = orig_endpoints.select("row_id").max().item()
 
-    return {"orig": orig_endpoints, "max_id": max_row_id}
+    return {
+        "orig": orig_endpoints,
+        "max_id": max_row_id,
+        "new_index_start": max_row_id + 1,
+    }
 
 
 snapshot = snapshot_acs_source()
@@ -48,33 +51,34 @@ def extend_acs(data: pl.DataFrame | pl.LazyFrame, years: list):
             base_len + 4,
         )
         .alias("end"),
-    ).collect()
-    
-    years_df = pl.DataFrame({"new_year":years})
+    )
 
-    # TODO create new row_id
-    # TODO only expand active rows
-    # TODO recreate url
-    expanded = base.join(years_df, how="cross")
-    new = expanded.filter(
-        (pl.col("year").cast(pl.Int64).ne(pl.col("new_year"))) & (pl.col("active").eq("1"))
-        ).drop("row_id").with_row_index("row_id", offset=snapshot["max_id"]+1).with_columns(
-            pl.concat_str(
-                [
-                    pl.col("begin"),
-                    pl.col("new_year"),
-                    pl.col("end")
-                ]
-            ).alias("endpoint"),
+    years_df = pl.DataFrame({"new_year": years}).lazy()
+
+    expanded = base.filter(pl.col("active").eq("1")).join(years_df, how="cross")
+
+    now = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    new = (
+        expanded.filter(pl.col("year").cast(pl.Int64).ne(pl.col("new_year")))
+        .drop("row_id")
+        .with_row_index("row_id", offset=snapshot["new_index_start"])
+        .with_columns(
+            pl.concat_str([pl.col("begin"), pl.col("new_year"), pl.col("end")]).alias(
+                "endpoint"
+            ),
             pl.col("new_year").alias("year"),
-            #pl.col("date_added") TODO set to now
-            pl.lit("").alias("date_last_pulled")
-        ).drop(
-            [
-                "begin",
-                "new_year",
-                "end"
-            ]
+            pl.lit(now).alias("date_added"),
+            pl.lit("").alias("date_last_pulled"),
         )
+        .drop(["begin", "new_year", "end"])
+        .select(
+            ["row_id", "endpoint", "active", "year", "date_added", "date_last_pulled"]
+        )
+        .collect()
+    )
 
-    return expanded
+    # TODO add year to data
+    stacked = pl.concat([data, new])
+
+    return stacked
