1804fa445df232c8b63d6d838a32a4c1d43e5603
Alexander Senetcky <Alexander.Senetcky@ct.gov>
2025-07-10 09:59:18 -0400

    feat: concat dataframes

--- src/incrementor/main.py
+++ src/incrementor/main.py
@@ -1,16 +1,6 @@
 from dataops import portal
-from dataops.models import CensusAPIEndpoint
 import polars as pl
 from datetime import datetime as dt
-# import urllib
-
-# take a look at incrementing the url years
-
-# take a look at the data catalog
-# api data catalog:  https://api.census.gov/data/
-
-# for each endpoint in endpoints use CensusAPIEndpoint.from_url()
-# construct a new instance but with a new year
 
 
 def snapshot_acs_source():
@@ -28,20 +18,16 @@ def snapshot_acs_source():
     max_row_id = orig_endpoints.select("row_id").max().item()
 
     return {
-        "orig": orig_endpoints,
-        "max_id": max_row_id,
-        "new_index_start": max_row_id + 1,
+        "df": orig_endpoints,
+        "id_start": max_row_id + 1,
     }
 
 
-snapshot = snapshot_acs_source()
-
-df = snapshot["orig"]
-
-
-def extend_acs(data: pl.DataFrame | pl.LazyFrame, years: list):
+def extend_acs(snapshot: dict, years: list[int]):
     api_census_base = "https://api.census.gov/data/"
     base_len = 28  # len(api_census_base)
+
+    data = snapshot["df"]
 
     base = data.lazy().with_columns(
         pl.lit(api_census_base).alias("begin"),
@@ -62,23 +48,41 @@ def extend_acs(data: pl.DataFrame | pl.LazyFrame, years: list):
     new = (
         expanded.filter(pl.col("year").cast(pl.Int64).ne(pl.col("new_year")))
         .drop("row_id")
-        .with_row_index("row_id", offset=snapshot["new_index_start"])
+        .with_row_index("row_id", offset=snapshot["id_start"])
         .with_columns(
             pl.concat_str([pl.col("begin"), pl.col("new_year"), pl.col("end")]).alias(
                 "endpoint"
             ),
-            pl.col("new_year").alias("year"),
             pl.lit(now).alias("date_added"),
-            pl.lit("").alias("date_last_pulled"),
+            pl.lit("1970-01-01 00:00:00").alias("date_last_pulled"),
         )
-        .drop(["begin", "new_year", "end"])
-        .select(
-            ["row_id", "endpoint", "active", "year", "date_added", "date_last_pulled"]
-        )
+        .drop(["begin", "year", "new_year", "end"])
+        .select(["row_id", "endpoint", "active", "date_added", "date_last_pulled"])
         .collect()
     )
 
-    # TODO add year to data
     stacked = pl.concat([data, new])
 
     return stacked
+
+
+def time_table(years: list[int] = None) -> list[int]:
+    if years is None:
+        end = dt.today().year - 1
+        start = end - 4
+
+        return list(range(start, end))
+
+    return years
+
+
+def main():
+    years = time_table()
+    snapshot = snapshot_acs_source()
+
+    huh = extend_acs(snapshot, years)
+    print(huh)
+
+
+if __name__ == "__main__":
+    main()
